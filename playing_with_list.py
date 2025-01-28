576a531f5e0000589baea660469063cd4b40b690
SanjuMu <sanjusri.munigala@gmail.com>
2025-01-27 19:45:00 -0500

    playing_with_list

---
+++ playing_with_list.py
@@ -0,0 +1,16 @@
+L = [4, 5, 1, 2, 9, 7, 10, 8]
+print("Oringinal List:", L)
+
+count = 0
+
+for i in L:
+    count += i
+
+avg= count/len(L)
+
+print("Sum=", count)
+print("Average=",avg)
+
+L.sort()
+print("Smallest element is", L[0])
+print("Largest element is", L[-1]) 
